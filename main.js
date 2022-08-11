e50121188a8e09f35aa51263eb57c2769c120899
thompso <95954959+thompso@users.noreply.github.com>
2022-08-11 21:41:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+var first_name = "Carl";
+console.log("first_name");   
+
+var last_name = "Mascarenhas";
+console.log("last_name");
+
+var full_name = first_name.concat(last_name);
+console.log("full_name"); 
+
+var name_one = "I Am John Rathod";
+console.log("name_one"); 
+
+var replace_name = name_one.replace("Rathod","Cena")
+console.log("replace_name"); 
+
+var capital_name = "ROCKY";
+var making_lower_case = capital.toLowerCase;
+
+var small_name = "john";
+var making_upper_case = capital.toUpperCase;
+
+var square_root = Math.sqrt(16);
+
+var maximum_number = Math.max(15, 10);
+
+var minimum_number = Math.min(15, 10);
+
+var power_eg_1 = math.pow(4, 2);
+
+var power_eg_2 = math.pow(4, 3);
